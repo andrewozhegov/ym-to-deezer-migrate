38de04b6ae4cad609c15a188c2311ab8d0834bd8
andrewozhegov <andrew.ozhegov@yandex.ru>
2019-05-03 19:43:07 +0300

    GO local OAuth2 server to get token from Deezer

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "os"
+    "fmt"
+	"net/http"
+    "io/ioutil"
+    "encoding/json"
+)
+
+var (
+    APP_ID     = "NULL"
+    SECRET_KEY = "NULL"
+)
+
+type DeezerTokenResponse struct {
+    Token string `json:"access_token"`
+    Expires int64 `json:"expires"`
+}
+
+func get_code (w http.ResponseWriter, r *http.Request) {
+    code, ok := r.URL.Query()["code"]
+    if !ok {
+        fmt.Printf("Can't get auth code!")
+        os.Exit(1)
+    } else {
+        res, err := http.Get("https://connect.deezer.com/oauth/access_token.php?app_id=" + string(APP_ID) + "&secret=" + string(SECRET_KEY) + "&code=" + string(code[0]) + "&output=json")
+        if err != nil {
+            fmt.Printf("Can't get token!")
+            os.Exit(1)
+        }
+        body, err := ioutil.ReadAll(res.Body)
+        var json_body = new(DeezerTokenResponse)
+        json.Unmarshal([]byte(body), &json_body)
+        fmt.Printf(json_body.Token)
+        fmt.Fprint(w, "Token saved successfully! You can close this tab")
+        os.Exit(0)
+    }
+}
+
+func main () {
+    http.HandleFunc("/", get_code)
+	http.ListenAndServe(":80", nil)
+}
+
